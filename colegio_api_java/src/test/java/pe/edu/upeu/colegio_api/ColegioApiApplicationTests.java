cc8630e4c083b67da014dcaad4f767d343a9a0e2
fiorellaPErezM <tu_correo_de_github@example.comfiorellapeerez608@gmail.com>
2026-07-05 00:49:00 -0500

    implemetar arquitectura de pruebas robusta y suite de validacion

---
+++ colegio_api_java/src/test/java/pe/edu/upeu/colegio_api/ColegioApiApplicationTests.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.colegio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
