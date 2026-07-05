64d245b0bb10e37af2d4a8ffc898e6ec1bda245c
Nikol235 <gemanikolticonalaura@gmail.com>
2026-07-05 04:04:18 -0500

    Visualización de la Página principal

---
+++ colegio_app/src/app/features/dashboard/ui/dashboard/dashboard.ts
@@ -0,0 +1,32 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { HttpClient } from '@angular/common/http';
+import { AuthState } from '../../../../core/auth-state';
+
+const BASE = 'http://localhost:8080/api/dashboard';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, CardModule],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss',
+})
+export class Dashboard implements OnInit {
+  authState = inject(AuthState);
+  private http = inject(HttpClient);
+
+  $data = signal<any>(null);
+
+  ngOnInit() {
+    const role = this.authState.$role();
+    if (role === 'admin' || role === 'director' || role === 'secretaria' || role === 'auxiliar') {
+      this.http.get(`${BASE}/admin`).subscribe(d => this.$data.set(d));
+    } else if (role === 'docente') {
+      this.http.get(`${BASE}/docente`).subscribe(d => this.$data.set(d));
+    } else if (role === 'padre') {
+      this.http.get(`${BASE}/padre`).subscribe(d => this.$data.set(d));
+    }
+  }
+}
